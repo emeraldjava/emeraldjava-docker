ec622f5574e4eb7f4f9a17b50e0c04a3acc9f50c
emeraldjava <emeraldjava@protonmail.com>
2023-04-05 11:16:11 +0100

    compose-oracle

--- compose-oracle-express/README.md
+++ compose-oracle-express/README.md
@@ -10,3 +10,5 @@ https://github.com/oracle/docker-images/issues/2599
 - https://github.com/Ameemsaeed/wildfly-app/blob/0d057e4214ca8b830a5932787f08e39330f11003/docker-compose.yaml
 - https://github.com/llucio/oracle-xe-ords-apex/blob/9b3131c822312f2615e1bd3862768975ea1e1efc/docker-compose.yaml
 - https://github.com/smallinsky/infra/blob/c1f86189e05ae36529e1266a1088a1df5b326bb5/oracle/Dockerfile
+
+- https://marschall.github.io/2020/04/12/oracle-docker-container.html
--- compose-oracle-express/setup/01_setup.sql
+++ compose-oracle-express/setup/01_setup.sql
@@ -1,3 +1,18 @@
+-- From https://marschall.github.io/2020/04/12/oracle-docker-container.html
+-- user.sql
+ALTER SESSION SET CONTAINER = XEPDB1;
+
+CREATE USER test_user IDENTIFIED BY "some-password";
+
+-- permissions.sql
+GRANT CONNECT TO test_user CONTAINER=CURRENT;
+GRANT CREATE SESSION TO test_user CONTAINER=CURRENT;
+GRANT RESOURCE TO test_user CONTAINER=CURRENT;
+ALTER USER test_user QUOTA 100M ON USERS;
+
+-- objects.sql
+ALTER SESSION SET CURRENT_SCHEMA = test_user;
+
 CREATE TABLE EMPLOYEE
 (
     ID INT ,
@@ -8,3 +23,5 @@ INSERT INTO EMPLOYEE VALUES(1,'Homer');
 INSERT INTO EMPLOYEE VALUES(2,'Bart');
 
 COMMIT;
+
+SELECT * FROM XEPDB1.EMPLOYEE;
